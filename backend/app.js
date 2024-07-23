1c88c6df8589761c2838ca87f0f48b5f0156d134
arye182 <arye.amsalem@gmail.com>
2024-07-23 20:40:00 +0300

    52-53-54

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 
 const app = express();
+
+// URL encode your password if it contains special characters
+const username = "devenvuser";
+const password = encodeURIComponent("MongoDb182");
+const cluster = "cluster0.wilzben";
+const dbname = "posts-full-stack";
+
+mongoose
+  .connect(
+    `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((error) => {
+    console.error("Connection failed: ", error);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,8 +40,12 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts/", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
   console.log(post);
+  post.save();
   res.status(201).json({ message: "post added successfuly!" });
 });
 
